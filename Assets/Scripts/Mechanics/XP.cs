ea9d8377a2068955e232ab4a57d619b4f65436d6
TomRC2 <117987302+TomRC2@users.noreply.github.com>
2024-12-11 12:31:57 -0300

    Fixed xp

--- Assets/Scripts/Mechanics/XP.cs
+++ Assets/Scripts/Mechanics/XP.cs
@@ -2,15 +2,13 @@ using UnityEngine;
 
 public class ExperienceOrb : MonoBehaviour
 {
-    public int experienceAmount = 10; // Cantidad de experiencia que otorga la orbe
+    public int experienceAmount = 10;
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("Player"))
         {
-            // Llama al método para otorgar experiencia al jugador
             collision.GetComponent<Player>().AddExperience(experienceAmount);
-            // Destruye la orbe
             Destroy(gameObject);
         }
     }
